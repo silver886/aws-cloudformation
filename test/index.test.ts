4bc2192fdd5e762ae002f58b3e359eb31fff5843
Leo Liu <karta1041337@gmail.com>
2021-03-02 07:00:21 +0800

    Fix unit test placeholder

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,6 +2,8 @@ import * as sdk from 'aws-sdk';
 import * as sdkMock from 'aws-sdk-mock';
 sdkMock.setSDKInstance(sdk);
 
+import * as src from '../src';
+
 describe('Initial custom resource', () => {
     describe('with creation event', () => {
         describe('when the secret is normal', () => {
